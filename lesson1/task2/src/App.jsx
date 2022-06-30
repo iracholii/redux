0c4500a18345da23d8641d1ca75b3a6f4577568a
iracholii <ira.cholii.m@gmail.com>
2022-06-30 17:49:34 +0300

    updating

--- lesson1/task2/src/App.jsx
+++ lesson1/task2/src/App.jsx
@@ -5,7 +5,7 @@ import Header from './Header';
 class App extends React.Component {
   state = {
     userData: {
-      name: 'Nicola Tesla',
+      name: 'Nikola Tesla',
       avatar_url: 'https://avatars3.githubusercontent.com/u10001',
     },
   };
