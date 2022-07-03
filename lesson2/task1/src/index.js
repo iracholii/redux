1ae846fa99652b6c8bb96d93026f94ea80327cbd
iracholii <ira.cholii.m@gmail.com>
2022-07-03 16:16:25 +0300

    adding files

---
+++ lesson2/task1/src/index.js
@@ -0,0 +1,22 @@
+import { createStore } from 'redux';
+
+export const increment = () => ({
+  type: 'COUNTER/INCREMENT',
+});
+
+export const decrement = () => ({
+  type: 'COUNTER/DECREMENT',
+});
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'COUNTER/INCREMENT':
+      return state + 1;
+    case 'COUNTER/DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(counterReducer);
